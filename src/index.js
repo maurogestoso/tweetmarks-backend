5245f494b5e356849033b04b5ae75a79bed7e360
Mauro Gestoso <mauro@Mauros-MacBook-Pro.local>
2019-01-29 21:51:07 +0000

    Get request oauth token

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import r from "request";
+import { promisify } from "util";
+import qs from "querystring";
+
+const postRequest = promisify(r.post);
+
+const {
+  PORT = 4000,
+  TWITTER_API_KEY,
+  TWITTER_API_SECRET_KEY,
+  TWITTER_ACCESS_TOKEN,
+  TWITTER_ACCESS_TOKEN_SECRET
+} = process.env;
+
+const TWITTER_API_BASE_URL = "https://api.twitter.com";
 
 const app = express();
 
@@ -14,7 +29,21 @@ app.get("/", (req, res) => {
   });
 });
 
-const { PORT = 4000 } = process.env;
+app.get("/sign-in", (req, res, next) => {
+  // Step 1: Obtain a request token
+  postRequest({
+    url: `${TWITTER_API_BASE_URL}/oauth/request_token`,
+    oauth: {
+      callback: "http://localhost:3000/callback",
+      consumer_key: TWITTER_API_KEY,
+      consumer_secret: TWITTER_API_SECRET_KEY
+    }
+  })
+    .then(response => {
+      res.send(qs.parse(response.body));
+    })
+    .catch(next);
+});
 
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on port ${PORT}`);
