f80a64a28b6e921d311daa8ca7bfdb9465b81534
Mauro Gestoso <mauro@Mauros-MacBook-Pro.local>
2019-02-10 08:28:02 +0000

    Complete Twitter sign in flow

---
+++ notes.md
@@ -0,0 +1,3 @@
+## Resources
+
+- [Implementing Sign In with Twitter](https://developer.twitter.com/en/docs/twitter-for-websites/log-in-with-twitter/guides/implementing-sign-in-with-twitter)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import "dotenv/config";
+
+import qs from "querystring";
+import path from "path";
+import { promisify } from "util";
+
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import bodyParser from "body-parser";
+
 import r from "request";
-import { promisify } from "util";
-import qs from "querystring";
 
 const postRequest = promisify(r.post);
 
@@ -20,17 +25,12 @@ const TWITTER_API_BASE_URL = "https://api.twitter.com";
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(morgan("dev"));
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send({
-    message: `You just requested ${req.url}`
-  });
-});
+app.use(bodyParser.json());
 
 app.get("/sign-in", (req, res, next) => {
-  // Step 1: Obtain a request token
   postRequest({
     url: `${TWITTER_API_BASE_URL}/oauth/request_token`,
     oauth: {
@@ -40,9 +40,39 @@ app.get("/sign-in", (req, res, next) => {
     }
   })
     .then(response => {
-      res.send(qs.parse(response.body));
+      const {
+        oauth_token,
+        oauth_token_secret,
+        oauth_callback_confirmed
+      } = qs.parse(response.body);
+
+      if (response.statusCode === 200) {
+        res.redirect(
+          302,
+          `${TWITTER_API_BASE_URL}/oauth/authenticate?oauth_token=${oauth_token}`
+        );
+      } else {
+        next(new Error("Request to /oauth/request_token failed"));
+      }
     })
     .catch(next);
+});
+
+app.get("/callback", (req, res, next) => {
+  const { oauth_token, oauth_verifier } = req.query;
+
+  postRequest({
+    url: `${TWITTER_API_BASE_URL}/oauth/access_token`,
+    oauth: {
+      verifier: oauth_verifier,
+      token: oauth_token
+    }
+  }).then(response => {
+    const { oauth_token, oauth_token_secret, user_id, screen_name } = qs.parse(
+      response.body
+    );
+    res.send({ sessionId: "123" });
+  });
 });
 
 app.listen(PORT, () => {
