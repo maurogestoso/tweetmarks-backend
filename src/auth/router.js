277d18f285df7e00b0edd937ba06116df93d9af4
Mauro Gestoso <maurogestoso@gmail.com>
2019-02-23 16:29:07 +0000

    Move sign-in and callback endpoints to auth router

--- src/auth/router.js
+++ src/auth/router.js
@@ -1,4 +1,17 @@
 import express from "express";
+import r from "request";
+import qs from "querystring";
+import { promisify } from "util";
+
+const {
+  TWITTER_API_KEY,
+  TWITTER_API_SECRET_KEY,
+  TWITTER_CALLBACK
+} = process.env;
+
+const TWITTER_API_BASE_URL = "https://api.twitter.com";
+
+const postRequest = promisify(r.post);
 
 const router = express.Router();
 
@@ -10,6 +23,54 @@ router.get("/", (req, res, next) => {
   }
 
   res.send();
+});
+
+router.get("/sign-in", (req, res, next) => {
+  return postRequest({
+    url: `${TWITTER_API_BASE_URL}/oauth/request_token`,
+    oauth: {
+      callback: TWITTER_CALLBACK,
+      consumer_key: TWITTER_API_KEY,
+      consumer_secret: TWITTER_API_SECRET_KEY
+    }
+  })
+    .then(twitterResponse => {
+      const { oauth_token } = qs.parse(twitterResponse.body);
+
+      res.redirect(
+        302,
+        `${TWITTER_API_BASE_URL}/oauth/authenticate?oauth_token=${oauth_token}`
+      );
+    })
+    .catch(next);
+});
+
+router.get("/callback", (req, res, next) => {
+  return postRequest({
+    url: `${TWITTER_API_BASE_URL}/oauth/access_token`,
+    oauth: {
+      verifier: req.query.oauth_verifier,
+      token: req.query.oauth_token
+    }
+  })
+    .then(twitterResponse => {
+      const {
+        oauth_token,
+        oauth_token_secret,
+        user_id,
+        screen_name
+      } = qs.parse(twitterResponse.body);
+
+      req.session.user = {
+        screen_name,
+        user_id,
+        oauth_token,
+        oauth_token_secret
+      };
+
+      res.send({ user: { user_id, screen_name } });
+    })
+    .catch(next);
 });
 
 export default router;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import "dotenv/config";
 
-import qs from "querystring";
 import path from "path";
-import { promisify } from "util";
 
 import express from "express";
 import cors from "cors";
@@ -10,21 +8,10 @@ import morgan from "morgan";
 import bodyParser from "body-parser";
 import session from "express-session";
 
-import r from "request";
-
 import authRouter from "./auth/router";
 import apiRouter from "./api/router";
 
-const postRequest = promisify(r.post);
-
-const {
-  PORT = 4000,
-  TWITTER_API_KEY,
-  TWITTER_API_SECRET_KEY,
-  SESSION_SECRET
-} = process.env;
-
-const TWITTER_API_BASE_URL = "https://api.twitter.com";
+const { PORT = 4000, SESSION_SECRET } = process.env;
 
 const app = express();
 
@@ -42,54 +29,6 @@ app.use(
 
 app.use("/api", apiRouter);
 app.use("/auth", authRouter);
-
-app.get("/sign-in", (req, res, next) => {
-  return postRequest({
-    url: `${TWITTER_API_BASE_URL}/oauth/request_token`,
-    oauth: {
-      callback: "http://localhost:3000/callback", // TODO: calculate programmatically
-      consumer_key: TWITTER_API_KEY,
-      consumer_secret: TWITTER_API_SECRET_KEY
-    }
-  })
-    .then(twitterResponse => {
-      const { oauth_token } = qs.parse(twitterResponse.body);
-
-      res.redirect(
-        302,
-        `${TWITTER_API_BASE_URL}/oauth/authenticate?oauth_token=${oauth_token}`
-      );
-    })
-    .catch(next);
-});
-
-app.get("/callback", (req, res, next) => {
-  return postRequest({
-    url: `${TWITTER_API_BASE_URL}/oauth/access_token`,
-    oauth: {
-      verifier: req.query.oauth_verifier,
-      token: req.query.oauth_token
-    }
-  })
-    .then(twitterResponse => {
-      const {
-        oauth_token,
-        oauth_token_secret,
-        user_id,
-        screen_name
-      } = qs.parse(twitterResponse.body);
-
-      req.session.user = {
-        screen_name,
-        user_id,
-        oauth_token,
-        oauth_token_secret
-      };
-
-      res.send({ user: { user_id, screen_name } });
-    })
-    .catch(next);
-});
 
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on port ${PORT}`);
