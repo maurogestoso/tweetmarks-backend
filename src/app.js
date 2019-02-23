797a31cdab14a9cb9e23a7ca5183467a0b63394a
Mauro Gestoso <maurogestoso@gmail.com>
2019-02-23 18:03:58 +0000

    Add CORS for frontend origin

--- src/app.js
+++ src/app.js
@@ -9,13 +9,17 @@ import session from "express-session";
 import authRouter from "./auth/router";
 import apiRouter from "./api/router";
 
-const { PORT = 4000, SESSION_SECRET } = process.env;
+const {
+  PORT = 4000,
+  SESSION_SECRET,
+  NODE_ENV = "development",
+  FRONTEND_BASE_URL
+} = process.env;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "../public")));
-app.use(morgan("dev"));
-app.use(cors());
+app.use(cors({ origin: FRONTEND_BASE_URL }));
 app.use(bodyParser.json());
 app.use(
   session({
